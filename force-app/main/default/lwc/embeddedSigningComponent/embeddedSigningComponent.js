c33e227429d7692412c8960622e082adb707dd37
Chung Byung Jae <jbj828@gmail.com>
2022-10-24 23:24:54 +0900

    feat: verify whether user grant consent by catching the consent error

--- force-app/main/default/lwc/embeddedSigningComponent/embeddedSigningComponent.js
+++ force-app/main/default/lwc/embeddedSigningComponent/embeddedSigningComponent.js
@@ -1,26 +1,35 @@
 import { LightningElement, api } from 'lwc';
 import sendEnvelope from '@salesforce/apex/EmbeddedSigningController.sendEnvelope';
 import getEmbeddedSigningUrl from '@salesforce/apex/EmbeddedSigningController.getEmbeddedSigningUrl';
+import errorHandler from '@salesforce/apex/ErrorHandler.errorHandler';
 
 export default class EmbeddedSigningComponent extends LightningElement {
 
-    template = '2712xxxx-xxxx-xxxx-xxxx-xxxxf9b8fa5f';
+    template = 'f65039ed-b1d7-4eaf-a3ad-eedd5133105c'; // 'One Signer' template
     description = 'Embedded Signing';
     @api recordId;
-    handleClick() {
-        sendEnvelope({template: this.template, description: this.description, recordId: this.recordId})
-            .then((envelopeId) => (
-                getEmbeddedSigningUrl({
-                    envId: envelopeId, 
+    async handleClick() {
+
+        try {
+            const res = await sendEnvelope({template: this.template, description: this.description, recordId: this.recordId});
+            console.log(res);
+
+            if(res.indexOf("APIException") == 0) {
+                // Other than consent error, throw error to show the error message on the browser console.
+                throw new Error(res);
+            } else if(res.indexOf("https://") == 0) {
+                // User needs to grant consent
+                window.open(res, '_blank');
+            } else {
+                const signingUrl = await getEmbeddedSigningUrl({
+                    envId: res, 
                     url: window.location.href
                 })
-            ))
-            .then((signingUrl) => {
                 window.location.href = signingUrl;
-            })
-            .catch((error) => {
-                console.log('Error:');
-                console.log(error);
-            });
+            }
+        } catch (error) {
+            console.log('Error:');
+            console.log(error);
+        }
     }
 }
